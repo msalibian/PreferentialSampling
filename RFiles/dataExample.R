56278efbfba84d9dbcbc99f054d0decd987e98bf
danieldinsdale <daniel.dinsdale@stat.ubc.ca>
2017-10-23 15:59:27 -0700

    Updated simple data generation example

--- RFiles/dataExample.R
+++ RFiles/dataExample.R
@@ -3,34 +3,34 @@ library(geoR)
 library(fields)
 # sample size
 n=100
-# choose parameters
-# choose preferential parameter (beta=0 is non-preferential)
-beta=2
+###########################################
+# Set field parameters ####################
+###########################################
+# choose preferential parameter (beta=0 is uniform random)
+beta=1.5
 # marginal variance of the field
 sigma.sq=1.5
 # nugget variance
 tau.sq=0.1
 # smoothness parameter
-kappa=1
-# scale
+nu=1
+# scale (range)
 phi=0.15
-# mean
+# mean parameter (constant mean trend)
 mean=4
-# set seed
-i=1
-set.seed(i)
-# define grid as 100 by 100 on the unit square
-xseq=seq(0,1,length.out=100)
-yseq=seq(0,1,length.out=100)
+# define grid as 91 by 91 on the unit square
+xseq=seq(0,1,length.out=91)
+yseq=seq(0,1,length.out=91)
 gridFull=expand.grid(xseq,yseq)
 # define covariance model for the field S
-model <- RMwhittle(nu=kappa, var=sigma.sq, scale=phi)
+model <- RMwhittle(nu=nu, var=sigma.sq, scale=phi)
 # generate the raw data for S
-rawDat <- RFsimulate(model, x=as.matrix(gridFull))$variable1 + mean
+set.seed(2)
+rawDat <- RFsimulate(model, x=as.matrix(gridFull), exactness=TRUE)$variable1 + mean
 # combine coordinates X with corresponding values for S
 obj <- cbind(cbind(gridFull[,1], gridFull[,2]), rawDat)
 geodata <- as.geodata(obj, coords.col = 1:2, data.col = 3)
-# sample the data according to PP with intensity exp(beta*S(x))
+# sample the data  with intensity exp(beta*S(x))/int(exp(beta*S(u))du)
 sampData <- sample.geodata(geodata, size = n, prob = exp(beta * geodata$data))
 # add nugget variance to Y's
 sampData$data <- sampData$data + rnorm(n, mean = 0, sd = sqrt(tau.sq))
